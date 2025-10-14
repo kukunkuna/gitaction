2a5aea2c0c463d91dbb9b3e5efa679059c617070
kukunkuna <ppadhi@gmail.com>
2025-10-14 12:11:09 -0500

    update security group

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ module "ec2_my_instance" {
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t3.micro"
   subnet_id     = "subnet-0ee2647685f6e2b5d"
+  vpc_security_group_ids="sg-0a5f66b2c030f2fd1"
   tags = {
     Name = "MyEC2Instance"
   }
