51464f25b54f6480b8efcc88ed0c46c5d64618ff
kukunkuna <48110409+kukunkuna@users.noreply.github.com>
2025-10-14 11:34:19 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
-  availability_zone = "subnet-0ee2647685f6e2b5d"
+  subnet_id = "subnet-0ee2647685f6e2b5d"
 tags = {
     Name = "MyEC2Instance"
   }
