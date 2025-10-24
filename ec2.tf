352a3683d8f564c76fa4fe1183cc97d7380b0b20
kukunkuna <48110409+kukunkuna@users.noreply.github.com>
2025-10-24 13:48:52 -0500

    Comment out EC2 instance module in ec2.tf
    Comment out the EC2 instance module configuration.

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,10 @@
-module "ec2_my_instance" {
-
-  source        = "./modules/ec2"
-  ami           = "ami-052064a798f08f0d3"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-0ee2647685f6e2b5d"
-  vpc_security_group_ids= ["sg-0a5f66b2c030f2fd1"]
-  tags = {
-    Name = "MyEC2Instance"
-  }
-}
+#module "ec2_my_instance" {
+#  source        = "./modules/ec2"
+#  ami           = "ami-052064a798f08f0d3"
+#  instance_type = "t2.micro"
+#  subnet_id     = "subnet-0ee2647685f6e2b5d"
+#  vpc_security_group_ids= ["sg-0a5f66b2c030f2fd1"]
+#  tags = {
+#    Name = "MyEC2Instance"
+#  }
+#}
